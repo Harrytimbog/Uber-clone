7bb51f7a547115bc7be8f6305441543eafbfedb7
Timilehin Arigbede <Harriemannie@gmail.com>
2022-06-29 20:58:44 +0100

    Building the Menu Button

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 // import tw from "twrnc";
 import tw from "tailwind-react-native-classnames";
@@ -7,13 +7,21 @@ import MapView from "react-native-maps";
 import { createStackNavigator } from "@react-navigation/stack";
 import NavigateCard from "../components/NavigateCard";
 import RideOptionsCard from "../components/RideOptionsCard";
+import { Icon } from "@rneui/base";
+import { useNavigation } from "@react-navigation/native";
 
 const MapScreen = () => {
   const Stack = createStackNavigator();
+  const navigation = useNavigation();
 
   return (
     <View style={{ flex: 1 }}>
-      <Text>Here is the MapScreen</Text>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("HomeScreen")}
+        style={tw`bg-gray-100 absolute top-16 left-8 z-50 p-3 rounded-full shadow-lg`}
+      >
+        <Icon name="menu" />
+      </TouchableOpacity>
       <View style={tw`h-1/2`}>
         <Map />
       </View>
